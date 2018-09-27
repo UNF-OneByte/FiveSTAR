cf4b809346ef6c8e0cb54ac0ae0735bad2d3168a
Shaun Young <1ShaunYoung1@gmail.com>
2018-09-27 13:47:32 -0400

    Created Task jobs, but still need to nest this within a specific project

---
+++ FiveSTAR/FiveSTAR_tracking/Models/Projects.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FiveSTAR_tracking.Models
+{
+	public class Projects
+	{
+		public string ID { get; set; }
+		public string Task { get; set; }
+		public DateTime Date { get; set; }
+		public decimal Hours { get; set; }
+		public decimal MaterialCost { get; set; }
+	}
+}
